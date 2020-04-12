729e0ac1583a7c1df0093b599f433dcf2451c079
awadnabeel <awadnabeel@hotmail.com>
2020-04-12 23:12:14 +0300

    add ping

--- main.py
+++ main.py
@@ -12,4 +12,5 @@ while True:
         end_ip = range[2]
         # print(network_part,start_ip,end_ip)
         datacollector = data_collector.DataCollector(network_part,start_ip,end_ip, 'admin', 'gn123125', 5)
+        time.sleep(10)
     time.sleep(60)
